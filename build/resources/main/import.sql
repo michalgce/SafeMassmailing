8c73b3c40ac19d767f80330dcf42b696075a2c4b
Michał Witkowski <michalgce@gmail.com>
2015-06-13 12:04:21 +0200

    CLEAN  UP

--- build/resources/main/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO subscriber(first_name, last_name, mail) VALUES ('Michał', 'Witkowski', 'michal@gmail.com');
-INSERT INTO subscriber(first_name, last_name, mail) VALUES ('Sandra', 'Kamela', 'sandra@gmail.com');
-
-INSERT INTO groups(name) VALUES ('Gliwice');
-INSERT INTO groups(name) VALUES ('Toszek');
-
-INSERT INTO smtp_configuration(address, name, password, port, user_name) VALUES ('localhost', 'Localhost SMTP server', null, '1025', null);
